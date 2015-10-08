353969f958178004ba5f4699f36acbeb26c45b90
sigvartmh <sigvart.m@gmail.com>
2015-10-08 13:05:20 +0200

    Finished ex3

--- ex6/specEst.m
+++ ex6/specEst.m
@@ -1,7 +1,28 @@
-f1 = 7/40; f2 = 9/40; N = 1024;
-%generates 100 samples of the sampled signal
-t = 0:99; x = sin(2*pi*f1*t) + sin(2*pi*f2*t); X = fft(x,N);
-f =0:1/N:1-1/N; figure(1)
-plot(f(1:length(f)/2),abs(X(1:length(f)/2)))
-xlabel('f');ylabel('|X(f)|')
-title('segment length 100 samples'); grid
+
+function specEst
+N = 1024;
+N_1 = 256;
+N_2 = 128;
+ f_1 = 7/40; 
+ f_2 = 9/40;
+ spec(100, N, f_1, f_2);
+ spec(1000, N, f_1, f_2);
+ spec(30, N, f_1, f_2);
+ spec(10, N, f_1, f_2);
+ spec(100, N_1, f_1, f_2);
+ spec(100, N_2, f_1, f_2);
+end
+
+function [ ] = spec(len, N, f_1, f_2)
+   
+    t=0:len-1;
+    x_n = sin(2*pi*f_1*t) + sin(2*pi*f_2*t); 
+    X_f = fft(x_n,N);
+    f =linspace(0, 1, length(X_f));
+    figure;
+    plot(f(1:length(f)/2),abs(X_f(1:length(f)/2)))
+    xlabel('f'); 
+    ylabel('|X(f)|');
+    title(sprintf('segment length %d samples and N=%d', len, N));
+    
+end
